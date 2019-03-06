f75564f54dd0efcca5b4fae71294a4ca7a0e433b
Anthony Da Mota <anthony.damota06@gmail.com>
2019-03-06 17:18:28 +0000

    Add entry point and server

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { routes } from "./routes";
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use("/", routes);
+  }
+}
+
+export default new App().app;
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import app from "./app";
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("User picker server running on " + PORT);
+});
